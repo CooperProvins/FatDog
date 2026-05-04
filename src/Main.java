543315c07e38e6ef2a42f0ca45e106f667205c16
Cooper Provins <cooperprovins@gmail.com>
2026-05-04 11:05:44 -0700

    Changed inputting, simplifeid breed names

--- src/Main.java
+++ src/Main.java
@@ -19,13 +19,13 @@ public class Main {
         Scanner input = new Scanner(System.in);
         DogReader.initialize();
 
-        System.out.println("What breed is your dog?");
+        System.out.print("What breed is your dog? ");
         dogBreedInput = input.nextLine();
         dogBreed = DogReader.findDogBreed(dogBreedInput);
-        System.out.println(dogBreed);
-        System.out.println("What sex is your dog?");
+        //System.out.println(dogBreed);
+        System.out.print("What sex is your dog? ");
         dogSex = Sex.parse(input.nextLine());
-        System.out.println("What is your dog's weight? (lbs)");
+        System.out.print("What is your dog's weight? (lbs) ");
         dogWeight = input.nextInt();
 
         percentile = dogBreed.weightPercentile(dogSex,dogWeight);

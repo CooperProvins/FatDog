d94549f60c9c39c3cd88a5ce0ecfd25116769685
Cooper Provins <cooperprovins@gmail.com>
2026-05-14 09:50:45 -0700

    fix commit

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,7 @@ public class Main {
         Scanner input = new Scanner(System.in);
         DogReader.initialize();
 
+<<<<<<< HEAD
         System.out.println("What breed is your dog?");
         dogBreedInput = input.nextLine();
         dogBreed = DogReader.findDogBreed(dogBreedInput);
@@ -26,6 +27,15 @@ public class Main {
         System.out.println("What sex is your dog?");
         dogSex = Sex.parse(input.nextLine());
         System.out.println("What is your dog's weight? (lbs)");
+=======
+        System.out.print("What breed is your dog? ");
+        dogBreedInput = input.nextLine();
+        dogBreed = DogReader.findDogBreed(dogBreedInput);
+        //System.out.println(dogBreed);
+        System.out.print("What sex is your dog? ");
+        dogSex = Sex.parse(input.nextLine());
+        System.out.print("What is your dog's weight? (lbs) ");
+>>>>>>> parent of 07cefc7 (File moving and deletion in app branch)
         dogWeight = input.nextInt();
 
         percentile = dogBreed.weightPercentile(dogSex,dogWeight);
